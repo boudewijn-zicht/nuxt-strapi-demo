d1f6f1a6c2cac1e5b5223a91677c558bae4f9d32
Boudewijn Schoon <boudewijn@zicht.nl>
2020-12-04 12:15:03 +0100

    Routing fixes

--- nuxt/apollo/config.js
+++ nuxt/apollo/config.js
@@ -1,6 +1,6 @@
 export default function () {
     return {
-        httpEndpoint: 'https://www.backend.test-t6dnbai-dimevesqs2roi.eu-4.platformsh.site/graphql',
+        httpEndpoint: 'https://backend.test-t6dnbai-dimevesqs2roi.eu-4.platformsh.site/graphql',
         // getAuth: () => '63010fddb21c316fbbe54794d51d00',
     };
 };
